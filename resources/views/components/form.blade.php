8323d4b9db4f8888213cf56f5b15d0c4fa68c5e6
Jefferson L Ferreira <jeffersonluan@gmail.com>
2025-07-07 13:12:26 -0300

    criacao de uma lista de email

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,16 @@
+@props([
+    'post' => null,
+])
+
+@php
+
+    $method = $post ? 'POST' : 'GET';
+
+@endphp
+
+<form {{ $attributes->class(['flex flex-col gap-4']) }} method="{{ $method }}">
+    @csrf
+
+    {{ $slot }}
+
+</form>
---
+++ resources/views/email-list/create.blade.php
@@ -0,0 +1,28 @@
+<x-layouts.app>
+    <x-slot name="header">
+        <x-h2>{{ __('Email List') }}</x-h2>
+    </x-slot>
+
+    <x-card>
+        <x-form :action="route('email-list.store')" post>
+
+            <div>
+                <x-input-label for="title" :value="__('Title')" />
+                <x-text-input id="title" class="block w-full mt-1" name="title" :value="old('title')" autofocus />
+                <x-input-error :messages="$errors->get('title')" class="mt-2" />
+            </div>
+
+            <div>
+                <x-input-label for="file" :value="__('File')" />
+                <x-text-input id="file" type="file" class="block w-full mt-1" name="file" autofocus />
+                <x-input-error :messages="$errors->get('file')" class="mt-2" />
+            </div>
+
+            <div class="flex items-center space-x-4">
+                <x-secondary-button type="reset">{{ __('Cancel') }}</x-secondary-button>
+                <x-primary-button type="submit">{{ __('Save') }}</x-primary-button>
+            </div>
+
+        </x-form>
+    </x-card>
+</x-layouts.app>
