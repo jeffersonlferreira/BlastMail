50407cabdf6d79a518436196c4e56ff9cca55893
Jefferson L Ferreira <jeffersonluan@gmail.com>
2025-07-08 14:14:12 -0300

    adicionar um assinante em uma lista

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\EmailList;
 use App\Models\Subscriber;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Illuminate\Database\Eloquent\Builder;
 
 
@@ -29,6 +30,23 @@ class SubscriberController extends Controller
             'search' => $search,
             'showTrash' => $showTrash,
         ]);
+    }
+
+    public function create(EmailList $emailList)
+    {
+        return view('subscriber.create', ['emailList' => $emailList]);
+    }
+
+    public function store(Request $request, EmailList $emailList)
+    {
+        $data = request()->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', Rule::unique('subscribers')->where('email_list_id', $emailList->id)]
+        ]);
+
+        $emailList->subscribers()->create($data);
+
+        return to_route('subscribers.index', $emailList)->with('message', __('Subscriber successfully created!'));
     }
 
     public function destroy(mixed $emailList, Subscriber $subscriber)
