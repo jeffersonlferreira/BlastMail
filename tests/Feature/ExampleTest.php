85db7434e5941014441cde3ea3da051d2d6d22f8
Jefferson L Ferreira <jeffersonluan@gmail.com>
2025-10-02 13:13:09 -0300

    removido erro no teste

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,11 +3,16 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\User;
 
 class ExampleTest extends TestCase
 {
     public function test_example()
     {
-        $this->assertDatabaseCount('users', 2);
+        $this->assertDatabaseCount('users', 0); // começa vazio
+
+        User::factory()->count(2)->create();
+
+        $this->assertDatabaseCount('users', 2); // agora deve ter 2
     }
 }
